7fe8b3bf12f3b55d0dceda49e2c176ecf97db6f4
Nicolas Guilherme <nicolas.gmop@gmail.com>
2018-07-09 20:21:32 -0300

    mais estudo adicionado :)

--- JavaScript ES6/aluraframe/client/js/app/controller/NegociacaoController.js
+++ JavaScript ES6/aluraframe/client/js/app/controller/NegociacaoController.js
@@ -8,10 +8,27 @@ class NegociacaoController{
         this.inputQuantidade = $("#quantidade");
         this.inputValor = $("#valor");
         
+        let self = this;
         
-        /*
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacoesView.update(model)); */
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            
+            get(target,prop,receiver){
+                
+                if(["adiciona", "esvazia"].includes(prop) && typeof target[prop] == "function"){
+                    
+                    return function(){
+                        
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                
+               return Reflect.get(target,prop,receiver);
+            }
+        });
+        
+        console.log(this._listaNegociacoes.adiciona);
         
         this._negociacoesView = new NegociacoesView($("#listaNegociacoes"));
         this._negociacoesView.update(this._listaNegociacoes);
