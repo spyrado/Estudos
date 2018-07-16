641d9c5ff4b0c1e7a8aae31ac4260e1326164f82
Nicolas Guilherme <nicolas.gmop@gmail.com>
2018-07-16 00:32:54 -0300

    mais conhecimento add :)

--- JavaScript ES6/aluraframe/client/js/app/controller/NegociacaoController.js
+++ JavaScript ES6/aluraframe/client/js/app/controller/NegociacaoController.js
@@ -8,32 +8,17 @@ class NegociacaoController{
         this.inputQuantidade = $("#quantidade");
         this.inputValor = $("#valor");
         
-        let self = this;
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($("#listaNegociacoes")),
+            "adiciona","esvazia"
+        );
         
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-            
-            get(target,prop,receiver){
-                
-                if(["adiciona", "esvazia"].includes(prop) && typeof target[prop] == "function"){
-                    
-                    return function(){
-                        
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                
-               return Reflect.get(target,prop,receiver);
-            }
-        });
-        
-        this._negociacoesView = new NegociacoesView($("#listaNegociacoes"));
-        this._negociacoesView.update(this._listaNegociacoes);
-        
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($("#mensagemView")),
+            "texto"
+        );
 
     }
     
@@ -43,8 +28,6 @@ class NegociacaoController{
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());        
         this._mensagem.texto = "Negociação adicionada com sucesso.";
-        this._mensagemView.update(this._mensagem);
-        
         this._limpaFormulario();     
     }
     
@@ -52,7 +35,6 @@ class NegociacaoController{
         
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = "Negociação apagada com sucesso!";
-        this._mensagemView.update(this._mensagem);
     }
     
     _criaNegociacao(){
---
+++ JavaScript ES6/aluraframe/client/js/app/services/ProxyFactory.js
@@ -0,0 +1,30 @@
+class ProxyFactory{
+
+    static create(objeto, props, acao){
+        
+        return new Proxy(objeto, {
+            
+            get(target,prop,receiver){
+                
+                if(props.includes(prop) && typeof target[prop] == "function"){
+                    
+                    return function(){
+                        
+                        Reflect.apply(target[prop], target, arguments);
+                        return acao(target);
+                    }
+                }
+                
+               return Reflect.get(target,prop,receiver);
+            },
+            set(target,prop,value,receiver){
+                if(props.includes(prop)){
+                    target[prop] = value;
+                    acao(target);
+                }
+                
+                return Reflect.set(target,prop,value,receiver);
+            }
+        });
+    }
+}
