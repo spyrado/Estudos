170abe6fe15b3df9334c14fca97f3d93f0a0a32f
Nicolas Guilherme <nicolas.gmop@gmail.com>
2018-11-19 10:22:34 -0200

    update com babel

--- JavaScript ES6/aluraframe/client/js/app/services/HttpService.js
+++ JavaScript ES6/aluraframe/client/js/app/services/HttpService.js
@@ -1,53 +1,70 @@
 class HttpService{
+
+    _handlerError(res){
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
     
     get(url){
         
-        return new Promise((resolve, reject) => {
+        return fetch(url)
+            .then(res => this._handlerError(res))
+            .then(res => res.json());
+        
+        // return new Promise((resolve, reject) => {
             
-            //criando uma instancia de XMLHttpRequest
-            let xhttp = new XMLHttpRequest();
+        //     //criando uma instancia de XMLHttpRequest
+        //     let xhttp = new XMLHttpRequest();
 
-            //preparando para abrir requisicao
-            xhttp.open('GET', url);
+        //     //preparando para abrir requisicao
+        //     xhttp.open('GET', url);
 
-            xhttp.onreadystatechange = () => {
+        //     xhttp.onreadystatechange = () => {
 
-                if(xhttp.readyState == 4){
-                    if(xhttp.status == 200){
-                        console.log("Obtendo as negociacoes do servidor");
-                        resolve(JSON.parse(xhttp.responseText));                            
-                    }else{
-                        console.log(xhttp.responseText);
-                        reject(xhttp.responseText);
-                    }
-                }
-            };
+        //         if(xhttp.readyState == 4){
+        //             if(xhttp.status == 200){
+        //                 console.log("Obtendo as negociacoes do servidor");
+        //                 resolve(JSON.parse(xhttp.responseText));                            
+        //             }else{
+        //                 console.log(xhttp.responseText);
+        //                 reject(xhttp.responseText);
+        //             }
+        //         }
+        //     };
 
-            //Enviando requisicao
-            xhttp.send();
-        });  
+        //     //Enviando requisicao
+        //     xhttp.send();
+        // });  
     }
     
     post(url, dado){
-        
-        return new Promise((resolve, reject) => {
-            
-            let xhttp = new XMLHttpRequest();
-        
-            xhttp.open('POST', url, true);
-            xhttp.setRequestHeader('Content-type', 'application/json');
-            xhttp.onreadystatechange = () => {
 
-                if(xhttp.readyState == 4){
-                    if(xhttp.status == 200){
-                        resolve(JSON.parse(xhttp.responseText));
-                    }else{
-                        reject(xhttp.responseText);
-                    }
-                }
-            }
+        return fetch(url, {
+            headers: {'Content-type': 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handlerError(res));
+        
+    //     return new Promise((resolve, reject) => {
             
-            xhttp.send(JSON.stringify(dado));
-        });
+    //         let xhttp = new XMLHttpRequest();
+        
+    //         xhttp.open('POST', url, true);
+    //         xhttp.setRequestHeader('Content-type', 'application/json');
+    //         xhttp.onreadystatechange = () => {
+
+    //             if(xhttp.readyState == 4){
+    //                 if(xhttp.status == 200){
+    //                     resolve(JSON.parse(xhttp.responseText));
+    //                 }else{
+    //                     reject(xhttp.responseText);
+    //                 }
+    //             }
+    //         }
+            
+    //         xhttp.send(JSON.stringify(dado));
+    //     });
+    // }
     }
 }
