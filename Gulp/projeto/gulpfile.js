5931739e1301474afc8cd47dcfaeed83df7dbd91
Nicolas Guilherme <nicolas.guilherme@mportal.com.br>
2018-12-05 09:10:41 -0200

    [UPDATE] Att conhecimento GULP

--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
+    clean = require('gulp-clean');
 
-gulp.task('build-img',function(){
-    gulp.src('src/img/**/*')
+/* Copia tudo da pasta src, e joga dentro de dist */
+gulp.task('copy', ['clean'],function(){
+    console.log('Copiando src e jogando e dist...');
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist')); // Caso n tenha dist, ele cria a pasta dist
+});
+
+/* Remove a pasta dist */
+gulp.task('clean', function(){
+    console.log('Removendo diretorio Dist...');
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+/* Minifica imagens da pasta dist (distribuição) */
+gulp.task('build-img', ['copy'],function(){
+    console.log('Minificando imagens...');
+    gulp.src('dist/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
+        .pipe(gulp.dest('dist/img'));
 });
