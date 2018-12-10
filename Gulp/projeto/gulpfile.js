80530a22e888e8e55ffbfc4b872df3d9da2b946c
Nicolas Guilherme <nicolas.guilherme@mportal.com.br>
2018-12-10 08:03:26 -0200

    [UPDATE] gulp

--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
-    clean = require('gulp-clean');
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace');
+
+/* Exucuta todas as minhas tarefas, copy com dependencia 
+E o restante assincronamente, isso é.. vao rodar ao msm tempo, pois 
+eles não dependem do outro */
+gulp.task('default',['copy'], function(){
+
+    gulp.start('build-img','build-js', 'build-html');
+});
 
 /* Copia tudo da pasta src, e joga dentro de dist */
 gulp.task('copy', ['clean'],function(){
-    console.log('Copiando src e jogando e dist...');
+    console.log('Copiando src e jogando em dist...');
     return gulp.src('src/**/*')
         .pipe(gulp.dest('dist')); // Caso n tenha dist, ele cria a pasta dist
 });
@@ -17,9 +27,28 @@ gulp.task('clean', function(){
 });
 
 /* Minifica imagens da pasta dist (distribuição) */
-gulp.task('build-img', ['copy'],function(){
+gulp.task('build-img',function(){
     console.log('Minificando imagens...');
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+});
+
+/* Concatena todos os meus arquivos js e joga pro arquivo js */
+gulp.task('build-js', function(){
+    console.log('Gerando arquivo all.js');
+    gulp.src(['./dist/js/jquery.js','./dist/js/home.js','./dist/js/produto.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+/* Concatena todos os meus arquivos js e joga pro arquivo js */
+gulp.task('build-html', ['build-js'],function(){
+    console.log('Removendo tags e adicionando uma unica tag all.js');
+    gulp.src('./dist/**/*.html')
+    .pipe(htmlReplace({
+        js: 'js/all.js'
+    }))
+    .pipe(gulp.dest('./dist/'));
+    
 });
