c3108215c5c6cc2c6883f960ecaf0ff75aea0385
Nicolas Guilherme <nicolas.guilherme@mportal.com.br>
2018-12-10 10:15:57 -0200

    [UPDATE] Estudos gulp

--- Gulp/projeto/gulpfile.js
+++ Gulp/projeto/gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
-    htmlReplace = require('gulp-html-replace');
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
 /* Exucuta todas as minhas tarefas, copy com dependencia 
 E o restante assincronamente, isso é.. vao rodar ao msm tempo, pois 
 eles não dependem do outro */
 gulp.task('default',['copy'], function(){
 
-    gulp.start('build-img','build-js', 'build-html');
+    gulp.start('build-img','usemin');
 });
 
 /* Copia tudo da pasta src, e joga dentro de dist */
@@ -34,21 +37,32 @@ gulp.task('build-img',function(){
         .pipe(gulp.dest('dist/img'));
 });
 
-/* Concatena todos os meus arquivos js e joga pro arquivo js */
-gulp.task('build-js', function(){
-    console.log('Gerando arquivo all.js');
-    gulp.src(['./dist/js/jquery.js','./dist/js/home.js','./dist/js/produto.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist/js'));
-});
+// /* Concatena todos os meus arquivos js e joga pro arquivo js */
+// gulp.task('build-js', function(){
+//     console.log('Gerando arquivo all.js');
+//     gulp.src(['./dist/js/jquery.js','./dist/js/home.js','./dist/js/produto.js'])
+//     .pipe(concat('all.js'))
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist/js'));
+// });
 
-/* Concatena todos os meus arquivos js e joga pro arquivo js */
-gulp.task('build-html', ['build-js'],function(){
-    console.log('Removendo tags e adicionando uma unica tag all.js');
-    gulp.src('./dist/**/*.html')
-    .pipe(htmlReplace({
-        js: 'js/all.js'
-    }))
-    .pipe(gulp.dest('./dist/'));
+// /* Concatena todos os meus arquivos js e joga pro arquivo js */
+// gulp.task('build-html', ['build-js'],function(){
+//     console.log('Removendo tags e adicionando uma unica tag all.js');
+//     gulp.src('./dist/**/*.html')
+//     .pipe(htmlReplace({
+//         js: 'js/all.js'
+//     }))
+//     .pipe(gulp.dest('./dist/'));
     
+// });
+
+gulp.task('usemin', function(){
+    
+    gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            'js': [uglify],
+            'css': [cssmin]
+        }))
+        .pipe(gulp.dest('dist'));
 });
