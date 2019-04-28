cb256e5885a4231145b32d29b1afbe5adb2e3d52
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-04-28 18:29:32 -0300

    att

---
+++ Angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo.interface';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'ap-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: Photo[] = [];
+
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ){}
+
+  ngOnInit(): void {
+
+    const userName = this.activatedRoute.snapshot.params.userName;
+    this.photoService
+      .listFromUser(userName)
+      .subscribe(photos => this.photos = photos);
+  }
+
+}
---
+++ Angular/alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+
+@NgModule({
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent
+  ],
+  imports: [ 
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [],
+  providers: [],
+})
+export class PhotosModule {}
