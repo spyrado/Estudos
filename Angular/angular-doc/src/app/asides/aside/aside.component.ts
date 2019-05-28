c4889b71c8293db184e6456a50ead4a2ccda352a
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-05-28 17:54:01 -0300

    att estudo

--- Angular/angular-doc/src/app/asides/aside/aside.component.ts
+++ Angular/angular-doc/src/app/asides/aside/aside.component.ts
@@ -24,12 +24,12 @@ export class AsideComponent implements OnInit{
     }
     
     ngOnInit(): void {
-      console.log(this.tollbarRef.nativeElement);
+      console.log(this.tollbarRef);
     }
     ngAfterViewInit(): void {
       //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
       //Add 'implements AfterViewInit' to the class.
-      console.log(this.tollbarRef.nativeElement);
+      console.log(this.tollbarRef.nativeElement.clientHeight);
       
     }
     configuracoesSideBar(){
