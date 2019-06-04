46767186f9586bae4fc54bc34988dfc6b49ac0b2
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-06-04 17:58:02 -0300

    [UPDATE] Adicionando  animacoes

--- Angular/angular-doc/src/app/forms/form-simples/form-simples.component.ts
+++ Angular/angular-doc/src/app/forms/form-simples/form-simples.component.ts
@@ -8,12 +8,18 @@ import { EventEmitter } from 'events';
 })
 export class FormSimplesComponent implements OnInit {
 
+
+  showSpinner: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void { }
 
   animaBotao(event){
+    this.showSpinner = true;
+    setTimeout(() => this.showSpinner = false, 700);
     event.preventDefault();
-    event.target.classList.add('hinge','animate');
+    event.target.classList.add('hinge','animate', 'btn-login-animate');
   }
+  // adicionar animacao ao botao com btn-login-animate
 }
