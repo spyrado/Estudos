2b259131ddc285074fd42a94d3683d5db60b09d7
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-06-28 16:37:35 -0300

    Criando um servidor com node.js

--- Node/casadocodigo/server.js
+++ Node/casadocodigo/server.js
@@ -1,1 +1,17 @@
-const http = require('http');
+const http = require('http'); // importo o modulo http
+
+const servidor = http.createServer((req,res) => {
+  res.end(
+    `
+      <html>
+          <head>
+              <meta charset="utf-8">
+          </head>
+          <body>
+              <h1> Casa do Código </h1>
+          </body> 
+      </html>
+  `
+  );
+}); // crio um servidor
+servidor.listen(3000) // digo ao servidor para rodar na porta 3000
