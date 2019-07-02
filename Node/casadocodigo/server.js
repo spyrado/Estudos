b463f91174f8d6d53d09d9299c293b5e75400486
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-07-02 08:37:29 -0300

    Adicionando rotas para as requisições

--- Node/casadocodigo/server.js
+++ Node/casadocodigo/server.js
@@ -1,17 +1,32 @@
 const http = require('http'); // importo o modulo http
 
 const servidor = http.createServer((req,res) => {
-  res.end(
-    `
-      <html>
-          <head>
-              <meta charset="utf-8">
-          </head>
-          <body>
-              <h1> Casa do Código </h1>
-          </body> 
-      </html>
-  `
-  );
+
+  let html = '';
+
+  if(req.url == '/'){
+    html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+        </html>
+    `;
+  }else if(req.url == '/livros'){
+    html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Listagem de Livros </h1>
+            </body> 
+        </html>
+    `;
+  }
+  res.end(html);
 }); // crio um servidor
 servidor.listen(3000) // digo ao servidor para rodar na porta 3000
