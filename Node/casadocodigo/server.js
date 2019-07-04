f94362bbd20edd9661ed0ac90c33b5be8d09880f
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-07-04 13:13:26 -0300

    Separando as responsabilidades no NODE

--- Node/casadocodigo/server.js
+++ Node/casadocodigo/server.js
@@ -1,32 +1,62 @@
-const http = require('http'); // importo o modulo http
+const app = require('./src/config/custom-express');
 
-const servidor = http.createServer((req,res) => {
+app.listen(3000, function(){ // Criando servidor
+  console.log('Servidor rodando na porta 3000');
+});
 
-  let html = '';
+// Ouvindo requisições da raiz e retornando uma resposta
+app.get('/', function(req,res){
+  res.send(`
+    <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Casa do Código </h1>
+        </body> 
+    </html>
+  `);
+});
+// Ouvindo requisições do livros e retornando uma resposta
+app.get('/livros', function(req,res){
+  res.send(`
+    <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Listagem de livros </h1>
+        </body> 
+    </html>
+  `);
+});
+// const servidor = http.createServer((req,res) => {
 
-  if(req.url == '/'){
-    html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-    `;
-  }else if(req.url == '/livros'){
-    html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Listagem de Livros </h1>
-            </body> 
-        </html>
-    `;
-  }
-  res.end(html);
-}); // crio um servidor
-servidor.listen(3000) // digo ao servidor para rodar na porta 3000
+//   let html = '';
+
+//   if(req.url == '/'){
+//     html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8">
+//             </head>
+//             <body>
+//                 <h1> Casa do Código </h1>
+//             </body> 
+//         </html>
+//     `;
+//   }else if(req.url == '/livros'){
+//     html = `
+//         <html>
+//             <head>
+//                 <meta charset="utf-8">
+//             </head>
+//             <body>
+//                 <h1> Listagem de Livros </h1>
+//             </body> 
+//         </html>
+//     `;
+//   }
+//   res.end(html);
+// }); // crio um servidor
+// servidor.listen(3000) // digo ao servidor para rodar na porta 3000
---
+++ Node/casadocodigo/src/config/custom-express.js
@@ -0,0 +1,4 @@
+const express = require('express'); // Recebendo uma função do express
+const app = express(); // Recebendo um objeto do express
+
+module.exports = app; // exportando minha função em node
