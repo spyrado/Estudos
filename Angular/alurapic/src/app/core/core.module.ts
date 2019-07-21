57f1180c010ee81f3b60bdcea67146ceb9a9a1f6
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-07-21 17:06:28 -0300

    adicionando footer

--- Angular/alurapic/src/app/core/core.module.ts
+++ Angular/alurapic/src/app/core/core.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [ HeaderComponent ],
+  declarations: [ 
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [ 
     CommonModule,
     RouterModule
   ],
-  exports: [ HeaderComponent ],
+  exports: [ 
+    HeaderComponent,
+    FooterComponent
+  ],
   providers: [],
 })
 export class CoreModule {}
