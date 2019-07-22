040f3c9750c7adfec23ebce8abf9fc18e0300b28
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-07-21 23:34:12 -0300

    att estudo angular

--- Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [
     PhotoFormComponent
   ],
-  imports: [ CommonModule ]
+  imports: [ 
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule,
+    FormsModule
+  ]
 })
 export class PhotoFormModule {}
