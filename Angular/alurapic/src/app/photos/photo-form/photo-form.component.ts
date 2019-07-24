2880b1f84f4828d8fa38bf1c7e093775ea386f70
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-07-23 23:37:51 -0300

    add estudo

--- Angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ Angular/alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PhotoService } from '../photo/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ap-photo-form',
@@ -9,13 +11,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
+  file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
-      file: ['', Validators.required]
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true]
     });
+  }
+
+  upload(){
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    console.log(description);
+    console.log(allowComments);
+    console.log(this.file);
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(
+        () => this.router.navigate(['']),
+        (error) => console.error(error)
+      );
   }
 
 }
