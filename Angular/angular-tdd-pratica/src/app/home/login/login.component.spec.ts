1904cd11266d8d6fcab66baf9919b8eb0d8e52e6
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-07-29 09:20:34 -0300

    add

---
+++ Angular/angular-tdd-pratica/src/app/home/login/login.component.spec.ts
@@ -0,0 +1,15 @@
+import { LoginComponent } from './login.component';
+
+
+describe('LoginComponent', () => {
+
+  beforeEach(() => loginComponent = new LoginComponent() );
+
+  it('should exist', () => {
+    expect(loginComponent).toBeTruthy();
+  });
+
+  it('O campo usuário deve ter apenas letras', () => {
+
+  });
+});
