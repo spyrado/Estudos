cf47d0069b440ae74bd954e1ca9b4e3df81f478d
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2019-07-30 15:17:06 -0300

    r4efazendo td node

--- Node/casadocodigo/server.js
+++ Node/casadocodigo/server.js
@@ -1,37 +1,5 @@
+const app = require('./src/app/configs/custom-express.js');
 
- 
-const app = require('./src/config/custom-express');
-
-app.listen(3000, function(){
-  console.log('Servidor iniciado.')
-});
-
-app.get('/',function(req,res){
-  res.send(
-  `
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Casa do Código </h1>
-        </body> 
-    </html>
-  `
-  );
-});
-
-app.get('/livros',function(req,res){
-  res.send(
-  `
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Listagem de Livros </h1>
-        </body> 
-    </html>
-  `
-  );
+app.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
 });
--- Node/casadocodigo/src/app/rotas/rotas.js
+++ Node/casadocodigo/src/app/rotas/rotas.js
@@ -1,30 +1,26 @@
 module.exports = (app) => {
-
-  // Ouvindo requisições da raiz e retornando uma resposta
-  app.get('/', function(req,res){
-    // Enviando a resposta para o cliente
+  app.get('/', (req, res) => {
     res.send(`
       <html>
-          <head>
-              <meta charset="utf-8">
-          </head>
-          <body>
-              <h1> Casa do Código </h1>
-          </body> 
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Casa do Código </h1>
+        </body> 
       </html>
     `);
   });
-  // Ouvindo requisições do livros e retornando uma resposta
-  app.get('/livros', function(req,res){
-    // Enviando a resposta para o cliente
+  
+  app.get('/livros', (req, res) => {
     res.send(`
       <html>
-          <head>
-              <meta charset="utf-8">
-          </head>
-          <body>
-              <h1> Listagem de livros </h1>
-          </body> 
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1> Listagem de livros </h1>
+        </body> 
       </html>
     `);
   });
