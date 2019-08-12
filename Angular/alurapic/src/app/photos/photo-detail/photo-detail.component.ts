f32299eea144a98acecda54e810c662a0dd037d6
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-08-11 23:32:31 -0300

    deixando parametro dinamico

--- Angular/alurapic/src/app/photos/photo-detail/photo-detail.component.ts
+++ Angular/alurapic/src/app/photos/photo-detail/photo-detail.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './photo-detail.component.html'
 })
 export class PhotoDetailComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id =  this.activatedRoute.snapshot.params.photoId;
+    console.log(id);
+   }
 }
