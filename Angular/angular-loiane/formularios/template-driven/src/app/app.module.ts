fc376de13d29aca73a1a12c0d6d3bd8443c700f6
Nicolas Blow Your Mind <nicolas.matheus@trust.com.br>
2019-10-16 14:45:33 -0300

    att estudo

--- Angular/angular-loiane/formularios/template-driven/src/app/app.module.ts
+++ Angular/angular-loiane/formularios/template-driven/src/app/app.module.ts
@@ -5,13 +5,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
   ],
   imports: [
     BrowserModule,
