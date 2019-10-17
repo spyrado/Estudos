66ba4ee8d0c75a63b55f29572a358828c0565f12
Nicolas Blow Your Mind <nicolas.matheus@trust.com.br>
2019-10-17 12:12:10 -0300

    add debugger

--- Angular/angular-loiane/formularios/template-driven/src/app/form-debug/form-debug.component.ts
+++ Angular/angular-loiane/formularios/template-driven/src/app/form-debug/form-debug.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form-debug',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-debug.component.scss']
 })
 export class FormDebugComponent implements OnInit {
+
+
+  @Input() form;
 
   constructor() { }
 
