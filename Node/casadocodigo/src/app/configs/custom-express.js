254d59004a626c5c5ca85941446d3d90433e5765
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-10-19 02:52:37 -0300

    att estudos

--- Node/casadocodigo/src/app/configs/custom-express.js
+++ Node/casadocodigo/src/app/configs/custom-express.js
@@ -4,6 +4,12 @@ require('marko/express');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+//Configurando para devolver o middleware que desejamos.
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const rotas = require('../rotas/rotas.js');
 rotas(app);
