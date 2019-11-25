c2c3d6360d81282b4353002021bd476343d6390a
Nicolas Guilherme <nicolas.gmop@gmail.com>
2019-11-24 23:11:39 -0300

    att estudo

--- Angular/alurapic/src/app/core/core.module.ts
+++ Angular/alurapic/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { RequestInterceptor } from './auth/request.interceptor';
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
 import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/directives/show-if-logged/show-if-logged.module';
 
 @NgModule({
   declarations: [ 
@@ -19,7 +20,8 @@ import { MenuModule } from '../shared/components/menu/menu.module';
     RouterModule,
     AlertModule,
     LoadingModule,
-    MenuModule
+    MenuModule,
+    ShowIfLoggedModule
   ],
   exports: [ 
     HeaderComponent,
