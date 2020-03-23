cc1f86562511a4656568d8d6ed2cbf4098ad59b5
Nicolas Guilherme <nicolas.gmop@gmail.com>
2020-03-23 00:47:48 -0300

    att estudos sobre flutter

--- Flutter/bytebank/lib/main.dart
+++ Flutter/bytebank/lib/main.dart
@@ -2,39 +2,63 @@
 
 import 'package:flutter/material.dart';
 
-void main() =>  runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Transferências'),
-      centerTitle: false,
-      backgroundColor: Color.fromRGBO(109, 33, 119, 1),
-    ),
-    body: ListaTransferencias(),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(
-        Icons.add
-      ),
-      backgroundColor: Color.fromRGBO(109, 33, 119, 1), 
-      onPressed: () {},
-    ),
-  ),
-));
+void main() => runApp(ByteBankApp());
 
-class  ListaTransferencias extends StatelessWidget {
+class ByteBankApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ItemTransferencia(
-        Transferencia(200, 1345)
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
-      ItemTransferencia(
-        Transferencia(345, 2665)
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+        centerTitle: false,
+        backgroundColor: Color.fromRGBO(109, 33, 119, 1),
       ),
-      ItemTransferencia(
-        Transferencia(7650, 8762)
+    );
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+        centerTitle: false,
+        backgroundColor: Color.fromRGBO(109, 33, 119, 1),
       ),
-    ],
+      body: Column(children: < Widget > [
+        ItemTransferencia(
+          Transferencia(200, 1345)
+        ),
+        ItemTransferencia(
+          Transferencia(345, 2665)
+        ),
+        ItemTransferencia(
+          Transferencia(7650, 8762)
+        ),
+      ], ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add
+        ),
+        backgroundColor: Color.fromRGBO(109, 33, 119, 1),
+        onPressed: () {},
+      ),
     );
   }
 
@@ -45,17 +69,17 @@ class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
- 
+
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: ListTile(
-          leading: Icon(
-            Icons.monetization_on
-          ),
-          title: Text(_transferencia.valor.toString()),
-          subtitle: Text(_transferencia.numeroConta.toString()),
-        )
+      child: ListTile(
+        leading: Icon(
+          Icons.monetization_on
+        ),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      )
     );
   }
 }
