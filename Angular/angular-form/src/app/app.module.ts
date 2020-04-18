44f694969d68b2b25a60c55f65bbc6d00235fabc
Nicolas Guilherme <nicolas.gmop@gmail.com>
2020-04-18 14:03:50 -0300

    att estudos

--- Angular/angular-form/src/app/app.module.ts
+++ Angular/angular-form/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormularioSimplesModule } from './formularios/formularios.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FormularioSimplesModule } from './formularios/formularios.module';
   ],
   imports: [
     BrowserModule,
-    FormularioSimplesModule
+    FormularioSimplesModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/angular-form/src/app/formularios/formulario-grupo/formulario-grupo.component.ts
+++ Angular/angular-form/src/app/formularios/formulario-grupo/formulario-grupo.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-grupo',
   templateUrl: './formulario-grupo.component.html'
 })
 export class FormularioGrupoComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void { }
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void { 
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.registerForm = this.formBuilder.group({
+      email: [null],
+      password: [null, Validators.required],
+      address: [null],
+      city: [null],
+      state: [null],
+      zip: [null],
+      checked: [false]
+    });
+  }
+
+  submitForm() {
+    console.log(this.registerForm);
+  }
 }
