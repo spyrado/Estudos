a88d23bf3b7688ed30cde8c348b87b0297f40aa8
Nicolas Guilherme <nicolas.gmop@gmail.com>
2020-05-24 16:19:21 -0300

    att estudos

--- Angular/angular-mock-api/src/app/mock/api/http-mock-api.interceptor.ts
+++ Angular/angular-mock-api/src/app/mock/api/http-mock-api.interceptor.ts
@@ -7,7 +7,7 @@ export class HttpMockApiInterceptor implements HttpInterceptor {
 
   intercept(req: import("@angular/common/http").HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
     const currentMockEndpoint = mockEndpoints[req.method] && mockEndpoints[req.method][req.url] || null;
-
+    console.log(req);
     return currentMockEndpoint ? currentMockEndpoint.handler() : next.handle(req);
   }
 
--- Angular/angular-mock-api/src/environments/environment.ts
+++ Angular/angular-mock-api/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  mockApi: false,
+  mockApi: true,
 };
 
 /*
